8139453a2c6a43b6c032e2e1f003fc186cdc80f4
GabrielVander <gabsvander@hotmail.com>
2020-10-02 09:22:18 -0300

    [ISSUE-8] Added snapshot test to DefaultApp

--- src/Components/DefaultApp/DefaultApp.test.tsx
+++ src/Components/DefaultApp/DefaultApp.test.tsx
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
     const linkElement = getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
 });
+
+test('matches snapshot', () => {
+    const component = render(<DefaultApp/>);
+
+    expect(component).toMatchSnapshot();
+})

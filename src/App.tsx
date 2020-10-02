9e59f04a44edca6a1339c280bf5f7c9ef1ee8f3f
GabrielVander <gabsvander@hotmail.com>
2020-10-02 09:30:04 -0300

    [ISSUE-8] Added routing logic to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import DefaultApp from "./Components/DefaultApp";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
     return (
         <>
-            <DefaultApp/>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/'>
+                        <DefaultApp/>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
         </>
     );
 }

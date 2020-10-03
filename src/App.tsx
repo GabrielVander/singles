d19e57d834e3431c3347d7d31ca778d26d740745
GabrielVander <gabsvander@hotmail.com>
2020-10-03 13:41:36 -0300

    [ISSUE-1] Initialized Firebase and added Redux dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,18 @@ import DefaultApp from "./Components/DefaultApp";
 import {BrowserRouter, Switch} from 'react-router-dom';
 import {HOME} from "./Routes/AppRoutes";
 import RouteWrapper from "./Components/RouteWrapper";
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/performance";
+import "firebase/auth";
+import "firebase/firestore";
+import firebaseConfig from "./Configurations/firebaseConfig";
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.performance();
+firebase.auth();
+firebase.firestore();
 
 function App() {
     return (

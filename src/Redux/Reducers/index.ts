44a57706945c38243644261d72c9d04372f54263
GabrielVander <gabsvander@hotmail.com>
2020-10-03 14:48:23 -0300

    [ISSUE-1] Configured Firebase + Redux

---
+++ src/Redux/Reducers/index.ts
@@ -0,0 +1,22 @@
+import {combineReducers} from "redux";
+import {FirebaseReducer, firebaseReducer, FirestoreReducer} from "react-redux-firebase";
+import {firestoreReducer} from "redux-firestore";
+
+interface Profile {
+    id: string
+    name: string
+    email: string
+}
+
+interface RootState {
+    firebase: FirebaseReducer.Reducer<Profile>;
+    firestore: FirestoreReducer.Reducer;
+}
+
+const rootReducer = combineReducers<RootState>({
+    firebase: firebaseReducer,
+    // @ts-ignore
+    firestore: firestoreReducer,
+});
+
+export default rootReducer;
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/performance";
+import "firebase/auth";
+import "firebase/firestore";
+import firebaseConfig from "./Configurations/firebaseConfig";
+import rrfConfig from "./Configurations/rrfConfig";
+import Store from "./Redux/Store";
+import {createFirestoreInstance} from "redux-firestore";
+import {Provider} from "react-redux";
+import {ReactReduxFirebaseProvider} from "react-redux-firebase";
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.performance();
+firebase.auth();
+firebase.firestore();
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <Provider store={Store.store}>
+            <ReactReduxFirebaseProvider
+                firebase={firebase}
+                config={rrfConfig}
+                dispatch={Store.store.dispatch}
+                createFirestoreInstance={createFirestoreInstance}
+            >
+                <App/>
+            </ReactReduxFirebaseProvider>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );

ba57d44ef62b00b2db925b34e21b44e354d903ea
GabrielVander <gabsvander@hotmail.com>
2020-10-04 11:54:45 -0300

    [ISSUE-18] Added LanguageDetector to i18n

--- src/Configurations/i18nConfig.ts
+++ src/Configurations/i18nConfig.ts
@@ -2,15 +2,20 @@ import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
 
 import Backend from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import Languages from "../Model/Languages";
 
 i18n
     .use(Backend)
     .use(initReactI18next)
+    .use(LanguageDetector)
     .init({
         debug: true,
+        detection: {
+            order: ['navigator', 'querystring', 'cookie', 'localStorage', 'htmlTag'],
+        },
 
-        lng: Languages.ENGLISH,
+        // lng: Languages.ENGLISH,
         fallbackLng: Languages.ENGLISH,
         whitelist: [Languages.ENGLISH],
 

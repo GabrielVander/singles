0a263e9734a4a4d47fa08fdec5cb4485e742cb90
GabrielVander <gabsvander@hotmail.com>
2020-10-04 12:57:02 -0300

    [ISSUE-18] Added basic snapshot test to CenteredSpin and fixed tests

--- src/Components/Others/CenteredSpin/CenteredSpin.test.tsx
+++ src/Components/Others/CenteredSpin/CenteredSpin.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {render} from '@testing-library/react';
-import DefaultApp from "./index";
+import CenteredSpin from "./index";
 
 test('matches snapshot', () => {
-    const component = render(<DefaultApp/>);
+    const component = render(<CenteredSpin/>);
 
     expect(component).toMatchSnapshot();
 })

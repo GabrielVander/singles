f7add7975b8df7a5a2a031376be799ea12d4290f
GabrielVander <gabsvander@hotmail.com>
2020-10-04 13:09:09 -0300

    [ISSUE-18] Fixed image position on DefaultApp

--- src/Components/Pages/DefaultApp/index.tsx
+++ src/Components/Pages/DefaultApp/index.tsx
@@ -9,12 +9,12 @@ function DefaultApp() {
     return (
         <div className="App">
             <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
                 <p>
                     <Trans i18nKey="defaultApp:editAndSave">
                         Edit <code>src/App.tsx</code> and save to reload.
                     </Trans>
                 </p>
-                <img src={logo} className="App-logo" alt="logo"/>
                 <a
                     className="App-link"
                     href="https://reactjs.org"

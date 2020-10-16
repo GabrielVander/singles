5b79753e06df7e126514fa7cc6303a3c0fe143d5
GabrielVander <gabsvander@hotmail.com>
2020-10-16 09:58:20 -0300

    [ISSUE-27] Refactored the Landing component

--- src/Components/Pages/Landing/Landing.spec.tsx
+++ src/Components/Pages/Landing/Landing.spec.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import {render} from '@testing-library/react';
-import DefaultApp from "./index";
-import Store from "../../../Redux/Store";
-
+import {render} from "@testing-library/react";
 import {Provider} from "react-redux";
+import Store from "../../../Redux/Store";
 import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import Landing from "./index";
 
 test('matches snapshot', () => {
     const component = render(
         <Provider store={Store.store}>
             <BrowserRouter>
-                <DefaultApp/>
+                <Landing/>
             </BrowserRouter>
         </Provider>
     );

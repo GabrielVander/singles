a32affea858edb40bb7bf59297defe68a8a64a60
Gabriel Vander <gabsvander@hotmail.com>
2020-10-20 09:22:25 -0300

    [ISSUE-27] Removed unused components

--- src/Components/Commom/CenteredSpin/CenteredSpin.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from '@testing-library/react';
-import CenteredSpin from "./index";
-
-test('matches snapshot', () => {
-    const component = render(<CenteredSpin/>);
-
-    expect(component).toMatchSnapshot();
-})

c5d63c755957bdf0f29b76f83e258569086d6b25
Gabriel Vander <gabsvander@hotmail.com>
2020-11-05 09:52:54 -0300

    [ISSUE-44] Added some fields to the Profile page (WIP)

--- src/Components/Pages/Profile/index.tsx
+++ src/Components/Pages/Profile/index.tsx
@@ -1,11 +1,144 @@
 import React from "react";
-import {Heading, Main, Paragraph} from "grommet";
+import {Box, DateInput, FormField, Heading, Main, Paragraph, Select, TextInput} from "grommet";
+import {Form, Formik} from "formik";
+import * as Yup from "yup";
 
 function Profile() {
+    const countries = ['Brazil', 'United States'];
+    const profileSchema = Yup.object({
+        fullName: Yup
+            .string()
+            .required('Name is required'),
+        birthday: Yup
+            .date()
+            .required('Birthday is required'),
+        country: Yup
+            .string()
+            .optional()
+            .oneOf(countries),
+        gender: Yup
+            .string()
+            .optional()
+            .oneOf(['Male', 'Female', 'Undefined'])
+    });
+
     return (
-        <Main pad="large">
-            <Heading>Something</Heading>
-            <Paragraph>Something about something</Paragraph>
+        <Main pad="large" align="center" gap="medium">
+            <Box align="center" border={{color: "brand", side: "all", size: "small"}} pad="small">
+                <Box round="large" background={{color: "brand"}} width="small" height="small">
+                    {/*<Image fit="contain" src="https://picsum.photos/200"/>*/}
+                </Box>
+                <Heading level="3">
+                    Username
+                </Heading>
+                <Paragraph>Description</Paragraph>
+            </Box>
+            <Box border={{color: "brand", side: "all", size: "small"}} fill="vertical" pad="small">
+                <Formik
+                    initialValues={{
+                        fullName: '',
+                        birthday: '01/01/2000',
+                        country: '',
+                        gender: '',
+                        languages: '',
+                        children: ''
+                    }}
+                    onSubmit={() => console.log('Submitted')}
+                    validationSchema={profileSchema}>
+                    {({
+                          errors,
+                          handleChange,
+                          handleBlur,
+                          handleSubmit
+                      }) => (
+                        <Form
+                            onSubmit={event => {
+                                event.preventDefault();
+                                handleSubmit();
+                            }}>
+                            <Box direction="row-responsive" fill="horizontal" gap="medium">
+                                <Box direction="column">
+                                    <FormField
+                                        error={errors.fullName}
+                                        label={'Full name'}>
+                                        <TextInput
+                                            placeholder={'Your full name'}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            name="fullName"/>
+                                    </FormField>
+                                    <FormField
+                                        error={errors.birthday}
+                                        label={'Birthday'}>
+                                        <DateInput
+                                            format="dd/mm/yyyy"
+                                            name="birthday"/>
+                                    </FormField>
+                                </Box>
+                                <Box direction="column">
+                                    <FormField
+                                        error={errors.country}
+                                        label={'Country'}>
+                                        <Select
+                                            placeholder={'Your current country'}
+                                            onChange={handleChange}
+                                            name="passwordConfirmation"
+                                            options={countries}/>
+                                    </FormField>
+                                    <FormField
+                                        error={errors.gender}
+                                        label={'Gender'}>
+                                        <Select
+                                            name="gender"
+                                            onChange={handleChange}
+                                            options={['Male', 'Female', 'Undefined']}/>
+                                    </FormField>
+                                </Box>
+                            </Box>
+
+                            {/*<Box*/}
+                            {/*    direction="row"*/}
+                            {/*    gap="medium"*/}
+                            {/*    justify="center"*/}
+                            {/*    margin="medium"*/}
+                            {/*>*/}
+                            {/*    <Button type="submit" primary label={t('register:registerButtonLabel')}/>*/}
+                            {/*</Box>*/}
+                            {/*<Box*/}
+                            {/*    direction="row"*/}
+                            {/*    justify="center"*/}
+                            {/*    align="center"*/}
+                            {/*    margin="small"*/}
+                            {/*>*/}
+                            {/*    <Paragraph margin="none">*/}
+                            {/*        {t('register:registerVia')}*/}
+                            {/*    </Paragraph>*/}
+                            {/*</Box>*/}
+                            {/*<Box*/}
+                            {/*    direction="row"*/}
+                            {/*    justify="around"*/}
+                            {/*>*/}
+                            {/*    <Button*/}
+                            {/*        onClick={registerWithGoogle}*/}
+                            {/*        icon={<Google color="plain"/>}/>*/}
+                            {/*    <Button*/}
+                            {/*        onClick={registerWithFacebook}*/}
+                            {/*        icon={<Facebook color="plain"/>}/>*/}
+                            {/*    <Button*/}
+                            {/*        onClick={registerWithTwitter}*/}
+                            {/*        icon={<Twitter color="plain"/>}/>*/}
+                            {/*</Box>*/}
+                            {/*<Box margin="small" align="center">*/}
+                            {/*    <Paragraph margin="none" textAlign="center">*/}
+                            {/*        <Trans i18nKey="register:register">*/}
+                            {/*            Already have an account? <Link to={LOGIN.path}>Login</Link>*/}
+                            {/*        </Trans>*/}
+                            {/*    </Paragraph>*/}
+                            {/*</Box>*/}
+                        </Form>
+                    )}
+                </Formik>
+            </Box>
         </Main>
     );
 }

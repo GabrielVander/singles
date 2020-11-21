de386951412ccea0da66c49539c9b72ffaae48e9
Gabriel Vander <gabsvander@hotmail.com>
2020-11-21 15:37:23 -0300

    [ISSUE-44] Updated saveUserToFirestore to assign a random profile image to the user + initial default data

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,28 +11,44 @@ import * as functions from 'firebase-functions';
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+async function getRandomFileFromDirectory(directory: string) {
+    const bucket = admin.storage().bucket();
+    const filesResult = await bucket.getFiles({directory});
+    const files = filesResult[0];
+    functions.logger.info(`Fetched ${files.length} files`);
+
+    const randomFile = files[(Math.floor(Math.random() * files.length))];
+    const [file] = await bucket.file(randomFile.name).makePublic();
+    return `${
+        file
+            .selfLink
+            .split('/acl')[0]
+            .replace('www.googleapis.com/storage', 'www.googleapis.com/download/storage')
+    }?generation=${file.generation}&alt=media`;
+}
+
 async function getRandomBackgroundImage() {
-    functions.logger.info('Fetching background images from Storage...');
+    functions.logger.info('Fetching background images from Firebase Storage...');
 
+    const directory = "background-images";
     try {
-        const bucket = admin.storage().bucket();
-        const filesResult = await bucket.getFiles({directory: "background-images"});
-        const files = filesResult[0];
-        functions.logger.info(`Fetched ${files.length} images`);
-
-        const randomFile = files[(Math.floor(Math.random() * files.length))];
-        const [file] = await bucket.file(randomFile.name).makePublic();
-        return `${
-            file
-                .selfLink
-                .split('/acl')[0]
-                .replace('www.googleapis.com/storage', 'www.googleapis.com/download/storage')
-        }?generation=${file.generation}&alt=media`;
+        return await getRandomFileFromDirectory(directory);
     } catch (e) {
-        functions.logger.error(`Something went wrong while attempting to fetch Storage resources:\n${e.message}`);
+        functions.logger.error(`Something went wrong while attempting to fetch background images from Firebase Storage:\n${e.message}`);
         return null;
     }
+}
 
+async function getRandomProfileImage() {
+    functions.logger.info('Fetching profile images from Firebase Storage...');
+
+    const directory = "profile-images";
+    try {
+        return await getRandomFileFromDirectory(directory);
+    } catch (e) {
+        functions.logger.error(`Something went wrong while attempting to fetch background images from Firebase Storage:\n${e.message}`);
+        return null;
+    }
 }
 
 export const saveUserToFirestore = functions
@@ -51,11 +67,19 @@ export const saveUserToFirestore = functions
                 displayName: user.displayName,
                 email: user.email,
                 emailVerified: user.emailVerified,
-                photoURL: user.photoURL,
+                photoURL: user.photoURL || await getRandomProfileImage(),
                 phoneNumber: user.phoneNumber,
                 lastLogin: admin.firestore.FieldValue.serverTimestamp(),
                 disabled: user.disabled,
-                backgroundImageURL: backgroundImage
+                backgroundImageURL: backgroundImage,
+                finishedSetup: false,
+                fullName: null,
+                dateOfBirth: null,
+                children: null,
+                gender: null,
+                description: null,
+                country: null,
+                spokenLanguages: null
             })
             .then(() => functions.logger.info(`User ${user.uid} saved successfully`))
             .catch((reason: any) => functions.logger.error(`Error while attempting to save user ${user.uid} to Firestore:\n${reason}`));

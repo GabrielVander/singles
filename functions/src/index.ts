5522c0c53f8c95bcea45d4ef45b7a90780f008a5
Gabriel Vander <gabsvander@hotmail.com>
2020-11-21 12:51:57 -0300

    [ISSUE-44] Updated saveUserToFirestore to assign a random backgroundURL to the user

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,24 +11,52 @@ import * as functions from 'firebase-functions';
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+async function getRandomBackgroundImage() {
+    functions.logger.info('Fetching background images from Storage...');
+
+    try {
+        const bucket = admin.storage().bucket();
+        const filesResult = await bucket.getFiles({directory: "background-images"});
+        const files = filesResult[0];
+        functions.logger.info(`Fetched ${files.length} images`);
+
+        const randomFile = files[(Math.floor(Math.random() * files.length))];
+        const [file] = await bucket.file(randomFile.name).makePublic();
+        return `${
+            file
+                .selfLink
+                .split('/acl')[0]
+                .replace('www.googleapis.com/storage', 'www.googleapis.com/download/storage')
+        }?generation=${file.generation}&alt=media`;
+    } catch (e) {
+        functions.logger.error(`Something went wrong while attempting to fetch Storage resources:\n${e.message}`);
+        return null;
+    }
+
+}
+
 export const saveUserToFirestore = functions
     .auth
     .user()
-    .onCreate((user) => {
-            functions.logger.info(`Saving user ${user.uid} to Firestore...`);
-            admin
-                .firestore()
-                .collection('users')
-                .doc(user.uid)
-                .set({
-                        displayName: user.displayName,
-                        email: user.email,
-                        emailVerified: user.emailVerified,
-                        photoURL: user.photoURL,
-                        phoneNumber: user.phoneNumber,
-                        lastLogin: admin.firestore.ServerValue.TIMESTAMP,
-                        disabled: user.disabled
-                })
-                .then(() => functions.logger.info(`User ${user.uid} saved successfully`))
-                .catch((reason: any) => functions.logger.error(`Error while attempting to save user ${user.uid} to Firestore:\n${reason}`));
+    .onCreate(async (user) => {
+        functions.logger.info(`Saving user ${user.uid} to Firestore...`);
+
+        const backgroundImage = await getRandomBackgroundImage();
+
+        admin
+            .firestore()
+            .collection('users')
+            .doc(user.uid)
+            .set({
+                displayName: user.displayName,
+                email: user.email,
+                emailVerified: user.emailVerified,
+                photoURL: user.photoURL,
+                phoneNumber: user.phoneNumber,
+                lastLogin: admin.firestore.FieldValue.serverTimestamp(),
+                disabled: user.disabled,
+                backgroundImageURL: backgroundImage
+            })
+            .then(() => functions.logger.info(`User ${user.uid} saved successfully`))
+            .catch((reason: any) => functions.logger.error(`Error while attempting to save user ${user.uid} to Firestore:\n${reason}`));
     });

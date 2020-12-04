d67ae933a04806140aa226cdc99a0850b0fa5e5f
Restyled.io <commits@restyled.io>
2020-12-04 12:40:07 +0000

    Restyled by clang-format

--- src/theme.js
+++ src/theme.js
@@ -1,29 +1,30 @@
-import { grommet } from 'grommet/themes';
-import { deepMerge } from 'grommet/utils';
+import {grommet} from 'grommet/themes';
+import {deepMerge} from 'grommet/utils';
 
 export const customTheme = deepMerge(grommet, {
-    global: {
-        breakpoints: {
-            xsmall: {
-                value: 400,
-            },
-        },
+  global : {
+    breakpoints : {
+      xsmall : {
+        value : 400,
+      },
     },
-    heading: {
-        // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-        extend: () => `margin-top: 12px; margin-bottom: 12px;`,
+  },
+  heading : {
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    extend : () => `margin-top: 12px; margin-bottom: 12px;`,
+  },
+  paragraph : {
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    extend : () => `font-weight: 300; margin-top: 0;`,
+    xxlarge : {
+      size : '28px',
     },
-    paragraph: {
-        // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-        extend: () => `font-weight: 300; margin-top: 0;`,
-        xxlarge: {
-            size: '28px',
-        },
+  },
+  textInput : {
+    placeholder : {
+      // eslint-disable-next-line
+      // @typescript-eslint/explicit-function-return-type
+      extend : () => `color: #44444`,
     },
-    textInput: {
-        placeholder: {
-            // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-            extend: () => `color: #44444`,
-        },
-    },
+  },
 });
